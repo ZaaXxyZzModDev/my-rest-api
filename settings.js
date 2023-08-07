d97b62f6c00d6ac5071817c1a80c96f7de19f483
ZaaXxyZz <130498570+ZaaXxyZzModDev@users.noreply.github.com>
2023-08-07 17:53:01 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,8 +3,8 @@ const fs = require('fs')
 global.creator = 'ZaaXxyZzModDev'// yourname
 global.MONGO_DB_URI = "mongodb+srv://zaax6677:rakaaja01kuy@cluster0.mgqay1v.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "BebasCughtt6282&#(_0_" //isi apa aja bebas
-global.your_email = "support@zaaxxyzzxd.site" //email
-global.email_password = "" //application password email
+global.your_email = "zaaxxyzzstorereall@gmail.com" //email
+global.email_password = "zjlldqbyismteuul" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
